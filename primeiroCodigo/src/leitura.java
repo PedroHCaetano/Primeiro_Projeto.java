54726a35e67f4bf02fcfc381acfb414906ebc9d1
PedroHCaetano <pedro.henricaetano1405@outlook.com>
2024-04-24 11:07:42 -0300

    repair name

--- primeiroCodigo/src/leitura.java
+++ primeiroCodigo/src/leitura.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class leitura {
     public static void main(String[] args) {
 
-        Scanner leitura = new Scanner(System.in);
+        Scanner Leitura = new Scanner(System.in);
         System.out.println("DIGITE SEU FILME FAVORITO: ");
-        String filme = leitura.nextLine();
+        String filme = Leitura.nextLine();
 
 
         System.out.println("Digite o ano de lançamento: ");
-        int anoDeLancamento = leitura.nextInt();
+        int anoDeLancamento = Leitura.nextInt();
 
         System.out.println("Avalia o filme de 0 a 10: ");
-        double avaliacao = leitura.nextDouble();
+        double avaliacao = Leitura.nextDouble();
 
         System.out.println(String.format("Você avaliou o filme %s, lançado em %d, em %.1f", filme, anoDeLancamento, avaliacao));
     }
